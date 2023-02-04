19da07fcc745c7badfeadd062cfd6ccbc91fd072
hahayz77 <danilo__danilo@hotmail.com>
2023-02-04 10:19:03 -0300

    RouterDataControl

--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -1,13 +1,22 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { RouterDataControl } from '../functions/RouterDataControl'
 
 const Context = createContext();
 
 export const StateContext = ( { children } ) => {
     const teste = "UseContext()!";
+    const [showCart, setShowCart] = useState(false);
+    const [cart, setCart] = useState([1,1,1,1,1]);
+    
+    RouterDataControl(setShowCart);
 
     return(
         <Context.Provider value={{
             teste,
+            showCart, 
+            setShowCart, 
+            cart, 
+            setCart,
         }}>
         {children}
         </Context.Provider>
---
+++ src/functions/RouterDataControl.js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+
+export function RouterDataControl(setShowCart){
+    const router = useRouter();
+    useEffect(()=>{
+        setShowCart(false);
+    },[router]);
+}
