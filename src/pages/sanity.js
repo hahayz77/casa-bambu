0799c9d6a8765a55603ddfc4ff7bcd5e86b4c4aa
hahayz77 <danilo__danilo@hotmail.com>
2023-02-16 06:26:48 -0300

    sanity works fine

---
+++ src/pages/sanity.js
@@ -0,0 +1,53 @@
+import { createClient } from "next-sanity";
+
+
+export default function Sanity({ pets }) {
+    return (
+      <>
+        <header>
+          <h1>Sanity + Next.js</h1>
+        </header>
+        <main>
+          <h2>pets</h2>
+          {pets.length > 0 && (
+            <ul>
+              {pets.map((pet) => (
+                <li key={pet._id}>{pet?.name}</li>
+              ))}
+            </ul>
+          )}
+          {!pets.length > 0 && <p>No pets to show</p>}
+          {pets.length > 0 && (
+            <div>
+              <pre>{JSON.stringify(pets, null, 2)}</pre>
+            </div>
+          )}
+          {!pets.length > 0 && (
+            <div>
+              <div>¯\_(ツ)_/¯</div>
+              <p>
+                Your data will show up here when you've configured everything
+                correctly
+              </p>
+            </div>
+          )}
+        </main>
+      </>
+    );
+  }
+
+  const client = createClient({
+    projectId: "u2y1sc0p",
+    dataset: "production",
+    apiVersion: "2022-03-25",
+    useCdn: false
+  });
+  
+  export async function getStaticProps() {
+    const pets = await client.fetch(`*[_type == "pet"]`);  
+    return {
+      props: {
+        pets
+      }
+    };
+  }
