0502ebba2499fdf975fec2383e0d6d2efa57ea01
hahayz77 <danilo__danilo@hotmail.com>
2023-03-02 08:33:59 -0300

    success page correction

--- src/pages/success.js
+++ src/pages/success.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 export default function Success() {
+    const email = 'contato@contato.com'
     return (
         <>
             <Nav/>
@@ -12,10 +13,11 @@ export default function Success() {
             <div id="success">
                 <h1 className="title">Tudo certo!</h1>
                 <Image src='/successbag.svg' width='300' height='300' />
-                <p className="!text-xl md:!text-2xl">Sua compra foi efetuada com sucesso!</p>
+                <p className="!text-xl md:!text-2xl !text-green">Sua compra foi efetuada com sucesso!</p>
                 <p>Agora é com a gente. Vamos preparar o seu pedido com carinho.</p>
                 <p>Você também pode confirmar esta compra no seu e-mail.</p>
                 <p>Se você tiver alguma outra dúvida é só falar...</p>
+                <p><a href={`mailto:${email}`}>{email}</a></p>
                 <div className="success_icons">
                     <FaWhatsapp/> 
                     <FaInstagram/> 
