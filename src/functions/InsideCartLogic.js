95d3a8948c01a0973377c5a32a228ab67532a1db
hahayz77 <danilo__danilo@hotmail.com>
2023-03-20 21:23:36 -0300

    2 decimals fixed

--- src/functions/InsideCartLogic.js
+++ src/functions/InsideCartLogic.js
@@ -1,34 +1,39 @@
-export function InsideCartLogic(product, method, cartItems, setCartItems, totalQuantities, setTotalQuantities, totalPrice, setTotalPrice, finalPrice, calcTotalPrice) {
+export async function InsideCartLogic(product, method, cartItems, setCartItems, totalQuantities, setTotalQuantities, totalPrice, setTotalPrice, finalPrice) {
 
-    calcTotalPrice = 0;
-    finalPrice = Number(parseFloat( product.price * ( 1- ( product.discount ) / 100) ).toFixed(2));
+    totalPrice = 0;
     const arrayOfOtherProducts = cartItems.filter((item) => item._id !== product._id);
     const index = cartItems.findIndex((e)=> e._id === product._id);
 
     if(method === 'dec'){
         if(product.qty > 1){
-            setTotalPrice((result) => result =  Number(parseFloat(totalPrice).toFixed(2)) - finalPrice);
             cartItems[index].qty -= 1;
 
         }else{
-            setTotalPrice((result) => result = Number(parseFloat(totalPrice).toFixed(2)) - finalPrice * product.qty);
             setCartItems(arrayOfOtherProducts);
         }
         // toast.error(`${product.name} Removed! `);
-        setTotalQuantities(() => totalQuantities - 1);
+        setTotalQuantities(totalQuantities - 1);
     }
     else if(method === 'inc'){ 																							// IMPLEMENTAR LIMITE DE PRODUTOS
-        setTotalQuantities(() => totalQuantities + 1);
-        setTotalPrice((result) => result = Number(parseFloat(totalPrice).toFixed(2)) + Number(parseFloat(finalPrice).toFixed(2)));
+        setTotalQuantities(totalQuantities + 1);
         cartItems[index].qty += 1;
         setCartItems(cartItems);
         // toast.success(`1un of ${product.name} added in Cart! `);
 
     }
     else if(method === 'rem'){
-        setTotalQuantities(() => totalQuantities - product.qty);
-        setTotalPrice(() => totalPrice - finalPrice * product.qty);
+        setTotalQuantities(totalQuantities - product.qty);
         setCartItems(arrayOfOtherProducts);
         // toast.error(`All ${product.name} Removed!`);
     }
+    
+    
+    for (let i in cartItems){
+        totalPrice += Number(parseFloat(cartItems[i].price * ( 1- ( cartItems[i].discount ) / 100)).toFixed(2) * cartItems[i].qty);
+    }
+    
+    setTotalPrice(Number(parseFloat(totalPrice).toFixed(2)));
+    console.log("-----------------------------")
+    console.log(product, method, cartItems, setCartItems, totalQuantities, setTotalQuantities, totalPrice, setTotalPrice, finalPrice);
+    console.log("-----------------------------")
 }
